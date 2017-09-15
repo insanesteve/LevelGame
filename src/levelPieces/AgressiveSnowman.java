5a41c8fd8c4995c31fb658456b84ea01869da46c
Stephen Agee <sagee@mines.edu>
2017-09-14 18:33:40 -0600

    Added some comments, tried debugging movement issues.

--- src/levelPieces/AgressiveSnowman.java
+++ src/levelPieces/AgressiveSnowman.java
@@ -16,21 +16,33 @@ public class AgressiveSnowman extends GamePiece implements Moveable, Drawable {
 
 	@Override
 	public void move(Drawable[] pieces, int playerLocation) {
-		int randInt = rand.nextInt(2);
+		System.out.println("Snowman wants to move!");
+		int randInt = rand.nextInt(1);
 		//move right
 		if (randInt == 0) {
-			if (playerLocation == super.getLocation() + 1) return;
-			
+			//if the player to the right of the snowman when attempting to move right, don't move
+			if (playerLocation == super.getLocation() + 1){
+				System.out.println("Snowman can't move!");
+				return;
+			}
+				//otherwise, if the snowman is not at the end of the board, move one space right
 				if (super.getLocation() != pieces.length - 1) {
+					//set the new location and swap it in the array
+					System.out.println("Snowman moves right!");
 					super.setLocation(super.getLocation() + 1);
 					swap(pieces, 1);
 				}
 		}
 		//move left
 		else {
-			if (playerLocation == super.getLocation() - 1) return;
+			//same structure applies here as above
+			if (playerLocation == super.getLocation() - 1){ 
+				System.out.println("Snowman can't move!");
+				return;
+			}
 			
 			if (super.getLocation() != 0) {
+				System.out.println("Snowman moves left!");
 				super.setLocation(super.getLocation() - 2);
 				swap(pieces, -1);
 			}
@@ -39,7 +51,7 @@ public class AgressiveSnowman extends GamePiece implements Moveable, Drawable {
 	
 		@Override
 		public InteractionResult interact(Drawable[] pieces, int playerLocation) {
-			
+			//if the player is one square away from the snowman, he will throw a snowball
 			if (Math.abs(playerLocation - super.getLocation()) == 1) {
 				System.out.println("The rather agressive snowman throws an agressive snowball at you... agressively");
 				return InteractionResult.HIT;
@@ -48,6 +60,7 @@ public class AgressiveSnowman extends GamePiece implements Moveable, Drawable {
 	}
 
 		@Override
+		//this function moves the character one space left or right in the array, as denoted by the changeMove argument
 		public void swap(Drawable[] pieces, int changeMove) {
 			Drawable temp = pieces[super.getLocation()];
 			pieces[super.getLocation()] = pieces[super.getLocation() + changeMove];
